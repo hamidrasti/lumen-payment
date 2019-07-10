2b5d81bec7c214aa6b193cc90674765cc102b197
Hamid Raasti <raasti.hamid@gmail.com>
2019-07-10 09:38:33 +0430

    Initial commit

---
+++ src/PaymentServiceProvider.php
@@ -0,0 +1,30 @@
+<?php
+
+namespace Hamraa\Payment;
+
+use Illuminate\Support\ServiceProvider;
+
+class PaymentServiceProvider extends ServiceProvider
+{
+
+    /**
+     * Bootstrap the application services.
+     *
+     * @return void
+     */
+    public function boot()
+    {
+        //
+    }
+
+    /**
+     * Register the application services.
+     *
+     * @return void
+     */
+    public function register()
+    {
+        //
+    }
+
+}
